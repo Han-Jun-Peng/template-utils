c54a6dfcd211f7a66035635a1019257bb08716a8
HanJunpeng <HanJunpeng@aliyun.com>
2024-10-25 16:36:52 +0800

    🐞 fix: 修改eslint的生效范围

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ export default [
 	{
 		...js.configs.recommended,
 		...tseslint.configs.recommended,
-		files: ["**/*.{js,mjs,cjs,ts}"],
+		files: ["**/src/**/*.{js,mjs,cjs,ts}"],
 		ignores: [
 			"**/*.cofig.{js,mjs,cjs,ts}",
 			"**/node_modules/**/*.{js,mjs,cjs,ts}",
