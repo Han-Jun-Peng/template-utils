0e2e140c4adcf0bf325797b2424180aa683b1ac7
HanJunpeng <HanJunpeng@aliyun.com>
2024-10-25 11:36:47 +0800

    🎉 init: 初始化eslint配置

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+	{
+		...js.configs.recommended,
+		...tseslint.configs.recommended,
+		files: ["**/*.{js,mjs,cjs,ts}"],
+		ignores: [
+			"**/*.cofig.{js,mjs,cjs,ts}",
+			"**/node_modules/**/*.{js,mjs,cjs,ts}",
+		],
+	},
+];
