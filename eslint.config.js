f95059ddbe1ee3b44effbdbda9ec5432529daec4
HanJunpeng <HanJunpeng@aliyun.com>
2024-10-26 10:48:45 +0800

    🦄 refactor: 修改eslint的控制范围

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,9 @@
 import js from "@eslint/js";
-import tseslint from "typescript-eslint";
 
 export default [
 	{
 		...js.configs.recommended,
-		...tseslint.configs.recommended,
-		files: ["**/src/**/*.{js,mjs,cjs,ts}"],
-		ignores: [
-			"**/*.cofig.{js,mjs,cjs,ts}",
-			"**/node_modules/**/*.{js,mjs,cjs,ts}",
-		],
+		files: ["./**/src/**/*.{js,mjs,cjs,ts}"],
+		ignores: ["./**/src/*.config.{js,mjs,cjs,ts}"],
 	},
 ];
